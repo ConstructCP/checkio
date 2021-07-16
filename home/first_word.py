a5a149978bc9670b30ac591fd2485bfceb42c625
Andrei Hubarevich1 <andrei_hubarevich1@epam.com>
2021-07-16 13:33:25 +0300

    first_word and right_to_left tasks

---
+++ home/first_word.py
@@ -0,0 +1,30 @@
+def first_word(text: str) -> str:
+    """
+        returns the first word in a given text.
+    """
+    word_beginning, word_end = None, None
+    for index, symbol in enumerate(text):
+        if word_beginning is None and symbol.isalpha():
+            word_beginning = index
+        if word_beginning is not None:
+            if symbol.isalpha() or symbol == "\'":
+                continue
+            else:
+                word_end = index
+                break
+    return text[word_beginning:word_end]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(first_word("Hello world"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert first_word("Hello world") == "Hello"
+    assert first_word(" a word ") == "a"
+    assert first_word("don't touch it") == "don't"
+    assert first_word("greetings, friends") == "greetings"
+    assert first_word("... and so on ...") == "and"
+    assert first_word("hi") == "hi"
+    assert first_word("Hello.World") == "Hello"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
---
+++ home/right_to_left.py
@@ -0,0 +1,23 @@
+def left_join(phrases: tuple) -> str:
+    """
+        Join strings and replace "right" to "left"
+    """
+    result = ''
+    for string in phrases:
+        replaced_string = string.replace('right', 'left')
+        if len(result) != 0:
+            result += ','
+        result += replaced_string
+    return result
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(left_join(("left", "right", "left", "stop")))
+
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert left_join(("left", "right", "left", "stop")) == "left,left,left,stop", "All to left"
+    assert left_join(("bright aright", "ok")) == "bleft aleft,ok", "Bright Left"
+    assert left_join(("brightness wright",)) == "bleftness wleft", "One phrase"
+    assert left_join(("enough", "jokes")) == "enough,jokes", "Nothing to replace"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
