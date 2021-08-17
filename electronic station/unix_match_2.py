8d3d1338cf4c4f720b3e9c56b1870c75ee16a771
Andrei Hubarevich <ahubarevich@ibagroup.eu>
2021-08-17 18:23:14 +0300

    edge cases for unix match 2

--- electronic station/unix_match_2.py
+++ electronic station/unix_match_2.py
@@ -38,7 +38,21 @@ def get_symbols_in_square_brackets(pattern_iter: Iterator) -> Tuple[Set, bool]:
     return char_set, is_excluding
 
 
+def handle_square_brackets_edge_cases(has_exclamation_mark, match_iter):
+    edge_case_seq = '[!]' if has_exclamation_mark else '[]'
+    for c in edge_case_seq:
+        file_char = get_next(match_iter)
+        if file_char != c:
+            raise RegexProcessingError
+
+
 def unix_match(filename: str, pattern: str) -> bool:
+    """
+    Check if filename matches pattern.
+    [seq] matches any character in seq, for example [123] means any character - '1', '2' or '3'
+    [!seq]	matches any character not in seq, for example [!123] means any character except '1', '2' and '3'
+    [] seq without any chars will never match
+    """
     pattern_it = iter(pattern)
     file_it = iter(filename)
     while True:
@@ -46,6 +60,10 @@ def unix_match(filename: str, pattern: str) -> bool:
             pattern_char = get_next(pattern_it)
             if pattern_char == '[':
                 char_set, is_excluding = get_symbols_in_square_brackets(pattern_it)
+                if len(char_set) == 0:
+                    # [] and [!] are not considered symbol sets
+                    handle_square_brackets_edge_cases(is_excluding, file_it)
+                    continue
                 file_char = get_next(file_it)
                 if (is_excluding and file_char in char_set) or (not is_excluding and file_char not in char_set):
                     raise RegexProcessingError
@@ -64,7 +82,7 @@ def unix_match(filename: str, pattern: str) -> bool:
 
 if __name__ == '__main__':
     print("Example:")
-    print(unix_match('somefile.txt', '*'))
+    print(unix_match('[!]check.txt', '[!]check.txt'))
 
     # These "asserts" are used for self-checking and not for an auto-testing
     assert unix_match('somefile.txt', 'somefile.txt') == True
